8300d9c4ad1b0b01932a1ccc8dd83f0ecd43ed20
EDK <kde9604@naver.com>
2020-05-31 16:00:28 +0900

    changeSet

--- src/geekos/sem.c
+++ src/geekos/sem.c
@@ -27,18 +27,19 @@
 #include <geekos/projects.h>
 #include <geekos/smp.h>
 
-#define MAX_SEM_SIZE 50
-#define MAX_NAME_SIZE 20
+#define MAX_SEM_SIZE 25
+#define MAX_NAME_SIZE 25
 
-struct Semaphore {
-    char* name;
+struct Semaphore
+{
+    char *name;
     int SID;
     int value;
     int num_users;
     struct Thread_Queue blockQueue;
 };
 
-static struct Semaphore* sem_list[MAX_SEM_SIZE];
+static struct Semaphore *sem_list[MAX_SEM_SIZE] = {0};
 
 /*
  * Create or find a semaphore.
@@ -48,53 +49,55 @@ static struct Semaphore* sem_list[MAX_SEM_SIZE];
  *   state->edx - initial semaphore count
  * Returns: the global semaphore id
  */
-int Sys_Open_Semaphore(struct Interrupt_State* state) {
-    KASSERT(state);             // may be removed; just to avoid compiler warnings in distributed code.
+int Sys_Open_Semaphore(struct Interrupt_State *state)
+{
+    KASSERT(state); // may be removed; just to avoid compiler warnings in distributed code.
     /*TODO_P(PROJECT_SEMAPHORES, "Open_Semaphore system call");
     return EUNSUPPORTED;*/
+    int i;
 
-    int len = state->ecx;
+    int length = state->ecx;
     int ival = state->edx;
 
-    // Name of semaphore is too long
-    if (len > MAX_NAME_SIZE)
+    if (length > MAX_NAME_SIZE)
+    {
         return ENAMETOOLONG;
+    }
 
-    char* name = Malloc(len + 1); // Holds name of semaphore
-    Copy_From_User(name, state->ebx, len + 1);
+    // Sys_PrintString Part
+    /*   state->ebx - user pointer of string to be printed
+     *   state->ecx - number of characters to print
+     *   Copy_User_String(state->ebx, length, 1023, (char **)&buf))
+     */
 
-    //struct Kernel_Thread *current = g_currentThreads;
+    char *name = Malloc(length + 1);
+    Copy_From_User(name, state->ebx, length + 1);
 
-    // Searches through global list of semaphores
-    int sem_ind = 0;
-    while (sem_list[sem_ind] != 0) {
-
-        // If the name exists
-        if (strncmp(sem_list[sem_ind]->name, name, len) == 0) {
-            sem_list[sem_ind]->num_users++;
-            Free(name);
-            return sem_list[sem_ind]->SID;
-        }
-
-        sem_ind++; // increment counter
-
-        // No more SIDs available and no semaphore with name exists
-        if (sem_ind == MAX_SEM_SIZE) {
+    for (i = 0; i != 0; i++)
+    {
+        if (i == MAX_SEM_SIZE)
+        {
             Free(name);
             return ENOSPACE;
+        }
+        if (strncmp(sem_list[i]->name, name, length) == 0)
+        {
+            sem_list[i]->num_users++;
+            Free(name);
+            return sem_list[i]->SID;
         }
     }
 
     // Create new semaphore and add to global semaphore list
-    struct Semaphore* new_sem = Malloc(sizeof(struct Semaphore));
+    struct Semaphore *new_sem = Malloc(sizeof(struct Semaphore));
     Clear_Thread_Queue(&new_sem->blockQueue);
     new_sem->name = name;
-    new_sem->SID = sem_ind;
+    new_sem->SID = i;
     new_sem->value = ival;
     new_sem->num_users = 1;
-    sem_list[sem_ind] = new_sem;
+    sem_list[i] = new_sem;
 
-    return sem_ind;
+    return i;
 }
 
 /*
@@ -106,27 +109,28 @@ int Sys_Open_Semaphore(struct Interrupt_State* state) {
  *
  * Returns: 0 if successful, error code (< 0) if unsuccessful
  */
-int Sys_P(struct Interrupt_State *state) {
-    KASSERT(state);             // may be removed; just to avoid compiler warnings in distributed code.
+int Sys_P(struct Interrupt_State *state)
+{
+    KASSERT(state); // may be removed; just to avoid compiler warnings in distributed code.
     /*TODO_P(PROJECT_SEMAPHORES, "P (semaphore acquire) system call");
     return EUNSUPPORTED;*/
-    int SID = state->ebx;
-    //struct Kernel_Thread* current = g_currentThread;
 
-    // Checks if valid SID or if current thread has sem with SID open
+    int SID = state->ebx;
+
     if (SID < 0 || SID >= MAX_SEM_SIZE || sem_list[SID] == 0)
+    {
         return EINVALID;
+    }
 
     bool iflag = Begin_Int_Atomic();
 
-    // If semaphore is available
-    if (sem_list[SID]->value > 0) {
+    if (sem_list[SID]->value > 0)
+    {
         sem_list[SID]->value--;
         End_Int_Atomic(iflag);
         return 0;
     }
 
-    // Wait on semaphore's wait queue
     Wait(&sem_list[SID]->blockQueue);
     iflag = Begin_Int_Atomic();
     sem_list[SID]->value--;
@@ -142,14 +146,14 @@ int Sys_P(struct Interrupt_State *state) {
  *
  * Returns: 0 if successful, error code (< 0) if unsuccessful
  */
-int Sys_V(struct Interrupt_State* state) {
-    KASSERT(state);             // may be removed; just to avoid compiler warnings in distributed code.
+int Sys_V(struct Interrupt_State *state)
+{
+    KASSERT(state); // may be removed; just to avoid compiler warnings in distributed code.
     /*TODO_P(PROJECT_SEMAPHORES, "V (semaphore release) system call");
     return EUNSUPPORTED;*/
-    int SID = state->ebx;
-    //struct Kernel_Thread* current = g_currentThreads;
 
-    // Checks if valid SID or if current thread has sem with SID open
+    int SID = state->ebx;
+
     if (SID < 0 || SID >= MAX_SEM_SIZE || sem_list[SID] == 0)
         return EINVALID;
 
@@ -171,8 +175,9 @@ int Sys_V(struct Interrupt_State* state) {
  *
  * Returns: 0 if successful, error code (< 0) if unsuccessful
  */
-int Sys_Close_Semaphore(struct Interrupt_State *state) {
-    KASSERT(state);             // may be removed; just to avoid compiler warnings in distributed code.
+int Sys_Close_Semaphore(struct Interrupt_State *state)
+{
+    KASSERT(state); // may be removed; just to avoid compiler warnings in distributed code.
     /*TODO_P(PROJECT_SEMAPHORES, "Close_Semaphore system call");
     return EUNSUPPORTED;*/
 
@@ -181,7 +186,8 @@ int Sys_Close_Semaphore(struct Interrupt_State *state) {
         return EINVALID;
 
     sem_list[SID]->num_users--;
-    if (sem_list[SID]->num_users == 0) {
+    if (sem_list[SID]->num_users == 0)
+    {
         Free(sem_list[SID]->name);
         Free(sem_list[SID]);
         sem_list[SID] = 0;
--- src/user/sem-p1.c
+++ src/user/sem-p1.c
@@ -16,18 +16,20 @@
 #include <sema.h>
 #include <string.h>
 
-int main(int argc __attribute__ ((unused)), char **argv
-         __attribute__ ((unused))) {
+int main(int argc __attribute__((unused)), char **argv
+         __attribute__((unused)))
+{
 
-    int i;                      /* loop index */
-    int scr_sem;                /* id of screen semaphore */
+    int i;       /* loop index */
+    int scr_sem; /* id of screen semaphore */
     int prod_sem, cons_sem;
 
-    scr_sem = Open_Semaphore("screen", 1);      /* register for screen use */
+    scr_sem = Open_Semaphore("screen", 1); /* register for screen use */
     prod_sem = Open_Semaphore("prod_sem", 0);
     cons_sem = Open_Semaphore("cons_sem", 1);
 
-    for(i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++)
+    {
         P(cons_sem);
         Print("Produced %d\n", i);
         V(prod_sem);
